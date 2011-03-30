b40ba95768324e311a7839f1c5160ca3ac69417d
root <root@hddtest.(none)>
2011-03-30 14:18:58 -0400

    Added drive blanking script

---
+++ blank_drives.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cat /root/BLANK_PARTITIONS | sfdisk /dev/sdc &
+cat /root/BLANK_PARTITIONS | sfdisk /dev/sdd &
+cat /root/BLANK_PARTITIONS | sfdisk /dev/sde &
+cat /root/BLANK_PARTITIONS | sfdisk /dev/sdf &
+cat /root/BLANK_PARTITIONS | sfdisk /dev/sdg &
+cat /root/BLANK_PARTITIONS | sfdisk /dev/sdh &
+cat /root/BLANK_PARTITIONS | sfdisk /dev/sdi &
+cat /root/BLANK_PARTITIONS | sfdisk /dev/sdj &
