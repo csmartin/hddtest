bfb8849bc1eb6b127e527cfd0d1ecb6ae5b45d6a
Grommit <grommit@middle-earth.us>
2011-06-28 16:44:48 -0400

    Added Nick Meisner script

---
+++ nm_testdrive.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Author: Carl Martin <cmartin@nexcess.net>
+# Tests a drive with badblocks and creates a log of it. Blinks drives when done, different if errors or not.
+# Usage:
+#    testdrive.sh sdf
+
+./testdrive.sh $1 nmeisner@nexcess.net
+
