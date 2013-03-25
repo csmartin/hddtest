6a212a33544c7cf89b2a9e38f1faf4beb1c82495
root <root@hddtest1.nexcess.net>
2013-03-25 16:43:33 -0400

    Added truncate log scripts

---
+++ cruise_control.sh
@@ -0,0 +1,1 @@
+/home/nexejozlin/cruise_control.sh
---
+++ truncate_logs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for item in $(find logs -name "*.log" -size +10M)
+do
+	head -n 1000 $item > $item.tmp
+	tail -n 1000 $item >> $item.tmp
+	mv $item.tmp $item
+	echo "Truncated $item to the first and last 1000 line"
+done
+
