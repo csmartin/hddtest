b73d0680600042c12dfee6b71f8f149dad3592a0
root <root@hddtest2.nexcess.net>
2013-04-10 13:18:52 -0400

    Adding subdirs. Updated hdparm to 9.43, e2fsprogs to 1.42.7, and smartmontools to 6.1

---
+++ sdparm-1.07/config.h
@@ -0,0 +1,68 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the `getopt_long' function. */
+#define HAVE_GETOPT_LONG 1
+
+/* Define to 1 if you have the <linux/bsg.h> header file. */
+#define HAVE_LINUX_BSG_H 1
+
+/* Define to 1 if you have the <linux/kdev_t.h> header file. */
+#define HAVE_LINUX_KDEV_T_H 1
+
+/* Define to 1 if you have the <linux/types.h> header file. */
+#define HAVE_LINUX_TYPES_H 1
+
+/* ignore linux bsg */
+/* #undef IGNORE_LINUX_BSG */
+
+/* Name of package */
+#define PACKAGE "sdparm"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "dgilbert@interlog.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "sdparm"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "sdparm 1.07"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "sdparm"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.07"
+
+/* sdparm Build Host */
+#define SG_LIB_BUILD_HOST "x86_64-unknown-linux-gnu"
+
+/* sdparm on FreeBSD */
+/* #undef SG_LIB_FREEBSD */
+
+/* assume sdparm on linux */
+#define SG_LIB_LINUX 1
+
+/* also MinGW environment */
+/* #undef SG_LIB_MINGW */
+
+/* sdparm on Tru64 UNIX */
+/* #undef SG_LIB_OSF1 */
+
+/* sdparm on Solaris */
+/* #undef SG_LIB_SOLARIS */
+
+/* sdparm on Win32 */
+/* #undef SG_LIB_WIN32 */
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "1.07"
+
+/* allow large buffers, aligned? */
+/* #undef WIN32_SPT_DIRECT */
